f932850865451e7017cefb8ff9d6a9ba2aa2e657
Sasha Postma <Scorching.Sammy@gmail.com>
2023-12-06 22:04:08 -0400

    Finished Project

---
+++ Assets/EndScreen.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class WinScreen : MonoBehaviour
+{
+    public GameObject GameOver;
+    public string targetTag = "Player";
+
+    void Start() {
+        GameOver.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(targetTag)) {
+            GameOver.SetActive(true);
+        }
+
+        Debug.Log("collided");
+    }   
+}
--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -6,12 +6,16 @@ using UnityEngine.Playables;
 public class Trigger : MonoBehaviour
 {
     public PlayableDirector timeline;
+    public GameObject trigger; 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             timeline.Play();
+            trigger.SetActive(false);
         }
+
+        Debug.Log("help");
     }
 }
